0fea62b729951418f947288b4c53a1417320d9ec
GreenKnight5417 <joshzeller25@gmail.com>
2024-03-07 18:17:23 -0500

    Volcano Enemy + Enemy Flinching
    Volcano Enemy + Enemy Flinching

--- GameDevGroup8/Assets/characters/EnemyHP.cs
+++ GameDevGroup8/Assets/characters/EnemyHP.cs
@@ -7,6 +7,7 @@ public class EnemyHP : MonoBehaviour
     public float hp;
     public GameObject enemy;
     public PlayerAttackDamage playerAttackHitbox;
+    public Animator animator;
 
     public PlayerSounds soundPlayer;
     //private int attackID = 0;
@@ -15,7 +16,7 @@ public class EnemyHP : MonoBehaviour
     void Start()
     {
         //playerAttackHitbox = GetComponent<PlayerAttackDamage>();
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -27,6 +28,15 @@ public class EnemyHP : MonoBehaviour
     public void damage(float dmg, bool crit)
     {
         hp -= dmg;
+        int randomStun = Random.Range(1, 100);
+        if (randomStun > 50)
+        {
+            int randomNumber = Random.Range(1, 3);
+            string tmp = "stunned" + randomNumber;
+            Debug.Log(tmp);
+            animator.Play(tmp);
+        }
+        
     }
 
     public float getHP()
